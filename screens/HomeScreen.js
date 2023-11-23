2ac60d16f422af07668f98c7f166b7652bdde56a
jurecapuder <capuder.j@gmail.com>
2023-11-23 19:08:32 +0100

    Add search functionality to HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,7 @@
-import { View, Text, SafeAreaView, Image } from 'react-native';
+import { View, Text, SafeAreaView, Image, TextInput, TouchableOpacity } from 'react-native';
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
+import { theme } from "../theme";
 
 export default function HomeScreen() {
   return (
@@ -12,6 +13,32 @@ export default function HomeScreen() {
         source={require('../assets/images/bg.png')}
         className="absolute h-full w-full"
       />
+
+      <SafeAreaView className="flex flex-1">
+        {/* Search section */}
+        <View
+          style={{height: '7%'}}
+          className="mx-4 relative z-50"
+        >
+          <View
+            className="flex-row justify-end items-center rounded-full"
+            style={{backgroundColor: theme.bgWhite(0.2)}}
+          >
+            <TextInput
+              placeholder="Search city"
+              placeholderTextColor={"lightgray"}
+              className="pl-6 h-10 flex-1 text-base text-white"
+            />
+
+            <TouchableOpacity
+              style={{backgroundColor: theme.bgWhite(0.3)}}
+              className="rounded-full p-3 m-1"
+            >
+              <Text>Icon</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </SafeAreaView>
     </View>
   )
 }
