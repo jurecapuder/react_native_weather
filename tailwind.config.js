dc327af5e9b2fe3e2323cb279b498de3bc865559
jurecapuder <capuder.j@gmail.com>
2023-11-23 18:42:35 +0100

    Fix formatting in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ module.exports = {
     "./App.{js,jsx,ts,tsx}", 
     "./screens/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
-    "./navigation/**/*.{js,jsx,ts,tsx}"],
+    "./navigation/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
     extend: {},
   },
