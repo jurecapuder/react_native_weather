4b35620cced288287c5d230e6c201c29828a5f68
jurecapuder <capuder.j@gmail.com>
2023-11-24 13:32:47 +0100

    Add search functionality to HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,13 @@
 import { View, Text, SafeAreaView, Image, TextInput, TouchableOpacity } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { theme } from "../theme";
 
+import { MagnifyingGlassIcon } from 'react-native-heroicons/outline';
+
 export default function HomeScreen() {
+  const [showSearch, toggleSearch] = useState(false);
+
   return (
     <View className="flex-1 relative">
       <StatusBar style="light" />
@@ -14,7 +18,7 @@ export default function HomeScreen() {
         className="absolute h-full w-full"
       />
 
-      <SafeAreaView className="flex flex-1">
+      <SafeAreaView className="flex flex-1 mt-10">
         {/* Search section */}
         <View
           style={{height: '7%'}}
@@ -24,17 +28,25 @@ export default function HomeScreen() {
             className="flex-row justify-end items-center rounded-full"
             style={{backgroundColor: theme.bgWhite(0.2)}}
           >
-            <TextInput
-              placeholder="Search city"
-              placeholderTextColor={"lightgray"}
-              className="pl-6 h-10 flex-1 text-base text-white"
-            />
+            {
+              showSearch ? (
+                <TextInput
+                  placeholder="Search city"
+                  placeholderTextColor={"lightgray"}
+                  className="pl-6 h-10 pb-1 flex-1 text-base text-white"
+                />
+              ) : null
+            }
 
             <TouchableOpacity
+              onPress={() => toggleSearch(!showSearch)}
               style={{backgroundColor: theme.bgWhite(0.3)}}
               className="rounded-full p-3 m-1"
             >
-              <Text>Icon</Text>
+              <MagnifyingGlassIcon
+                size="25"
+                color="white"
+              />
             </TouchableOpacity>
           </View>
         </View>
