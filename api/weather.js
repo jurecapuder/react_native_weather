e48f4618d33ce5ac2d59bf62b12b65b83d038f87
jurecapuder <capuder.j@gmail.com>
2023-11-26 12:33:54 +0100

    Add forecast and locations endpoints

--- api/weather.js
+++ api/weather.js
@@ -4,3 +4,7 @@ import { apiKey } from "../constants";
 const forecastEndpoint = params => {
   `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${params.cityName}&days=${params.days}&aqi=no&alerts=no`
 }
+
+const locationsEndpoint = params => {
+  `https://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${params.cityName}`
+}
