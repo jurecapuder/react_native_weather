e5a9e0c1517590ba4d84353df4bb56472917534e
jurecapuder <capuder.j@gmail.com>
2023-11-26 12:35:30 +0100

    Add API call function for weather endpoint

--- api/weather.js
+++ api/weather.js
@@ -8,3 +8,20 @@ const forecastEndpoint = params => {
 const locationsEndpoint = params => {
   `https://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${params.cityName}`
 }
+
+const apiCall = async (endpoint) => {
+  const options = {
+    method: 'GET',
+    url: endpoint,
+  };
+
+  try {
+    const response = await axios.request(options);
+
+    return response.data;
+  } catch (error) {
+    console.log(error);
+
+    return null;
+  }
+}
